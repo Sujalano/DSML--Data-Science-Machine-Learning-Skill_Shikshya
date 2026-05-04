3a1b947a659142ca8750d9565876fadfa2efe69c
Sujal Shrestha <119942963+Sujalano@users.noreply.github.com>
2026-05-04 01:47:27 +0000

    Updated

--- Assignments/Assignment_03/project_03.py
+++ Assignments/Assignment_03/project_03.py
@@ -74,7 +74,7 @@ result_mult = Demin_02 * 2
 print(result_mult)
 A = np.array([[1,2],[3,4]])
 B = np.array([[5, 6], [7, 8]])
-matrix_product= A @ B
+matrix_product= np.dot(A,B)
 print(matrix_product)
 
 """
